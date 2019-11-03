b6bc0ca21f2070834813e6e17dd6f8f6b0d0156b
ngodi <albertngodi@gmail.com>
2019-11-03 20:03:50 +0100

    get user location by default

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import getWeather from './modules/data/weather';
+import {  messagesElement, weather_iconElement, temperatureElement,
+   descriptionElement, locationElement} from './modules/ui/elements';
 
 getWeather('Canada');
+
+const getCurrentLocation = () => {
+  if(geoLocation in navigator){
+   return  navigator.geolocation.getCurrentPosition(setPosition, showError);
+  }else{
+  notificationElement.style.display = "block";
+  notificationElement.innerHTML = "error";
+  }
+};
+
+
+
+const setPosition = (position) => {
+return location;
+}
+
+
+const showerror = (error) => {
+notificationElement.style.display = 'block';
+notificationElement.innerHTML = 'error';
+};
---
+++ src/modules/ui/elements.js
@@ -0,0 +1,9 @@
+let messagesElement = document.querySelector('.messages');
+let weather_iconElement = document.querySelector('.weather-icon') ;
+let temperatureElement = document.querySelector('.temperature');
+let descriptionElement = document.querySelector('.description');
+let locationElement = document.querySelector('.location');
+
+export {
+    messagesElement, weather_iconElement, temperatureElement, descriptionElement, locationElement
+}
